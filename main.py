f88ed51f1c369a266982128d09facf918e2bca4c
michalszmigiel <michal.szmigiel@tsh.io>
2025-03-26 23:12:18 +0100

    L4

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class Window():
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -25,8 +28,31 @@ class Window():
         self.__running = False
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 
